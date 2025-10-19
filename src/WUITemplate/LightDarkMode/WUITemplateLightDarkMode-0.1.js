7e002904e4913712876020039e091338cb7d9000
Sergio Belmar <sbelmar@gmail.com>
2025-10-19 12:13:53 -0300

    backup

---
+++ src/WUITemplate/LightDarkMode/WUITemplateLightDarkMode-0.1.js
@@ -0,0 +1,21 @@
+/*
+ * WUITemplateLightDarkMode - v0.1
+ * Author: Sergio E. Belmar (sbelmar@1640lab.com)
+ * Distributed by: 1640 Lab S.p.A. (https://1640lab.com)
+ * Copyright (c) Sergio E. Belmar (sbelmar@1640lab.com)
+ */
+
+class WUITemplateLightDarkMode {
+
+	static version = "0.1";
+
+	getScheme() {
+		return tgetComputedStyle(document.documentElement).getPropertyValue("color-scheme").trim();
+	}
+
+	setScheme(value) {
+		if (typeof(value) == "string" && value.match(/^(light|only light|dark|only dark|light dark|dark light)$/i)) {
+			document.documentElement.style.colorScheme = value.toLowerCase();
+		}
+	}
+}
